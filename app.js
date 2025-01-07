123b9adc384022dfb858756421061759073e717c
YUVRAJSINGH3112 <yuvrajsingh3112s@gmail.com>
2025-01-07 22:26:45 +0530

    some random updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const path=require('path');
 const db=require('./config/mongoose-connection');
 const userRoutes=require("./routes/userRoute");
+const home=require("./routes/homeRoute");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
@@ -10,7 +11,7 @@ app.use(express.static(path.join(__dirname,"public")));
 app.set('view engine', 'ejs');
 
 app.use("/user", userRoutes);
-
+app.use("/", home);
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,30 +3,46 @@ const router=express.Router();
 const userModel=require("../models/user");
 const bcrypt = require("bcrypt");
 const cookie=require("cookie-parser")
+const jwt=require("jsonwebtoken");
 
-router.get("/create",(req,res)=>{
+
+
+router.get("/signup",(req,res)=>{
     res.render("signup.ejs");
 });
 
-router.post("/create",(req,res)=>{
+
+
+router.post("/signup",async (req,res)=>{
+
     if(req.body.password===req.body.confirmPassword){
-        let{fullname, email, password}=req.body;
-    
-    bcrypt.genSalt(10,(err,salt)=>{
-        if(err) return res.error(err);
-       bcrypt.hash(password,salt,async (err,hash)=>{
-        if(err) return res.error(err);
-            let createdUser=await userModel.create({
-                fullname,
-                email,
-                password:hash
-            });
-            res.send(createdUser);
-       })
-    });
+
+    let{fullname, email, password}=req.body;
+    const existedUser=await userModel.findOne({email});
+    if(existedUser){
+        return res.status(400).send("Email already exists");
     }
     else{
-        res.send("Passwords and confirm password do not match");
+        bcrypt.genSalt(10,(err,salt)=>{
+            if(err) return res.error(err);
+           bcrypt.hash(password,salt,async (err,hash)=>{
+            if(err) return res.error(err);
+                let createdUser=await userModel.create({
+                    fullname,
+                    email,
+                    password:hash
+                });
+                let token= jwt.sign({email},"poloololo");
+                res.cookie=("token",token);
+                res.status(201).render("home.ejs");
+           })
+        });
+    
+    }
+    }
+
+    else{
+       res.status(400).send("Passwords and confirm password do not match");
     }
 });
 
